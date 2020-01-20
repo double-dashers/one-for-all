3876d1228f190184f3c6953b768db190886d3175
Daniel <danielceballos94@gmail.com>
2020-01-20 10:56:52 -0500

    Created grid layout with placeholder images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Menu } from "semantic-ui-react";
+import { Menu, Grid, Image } from "semantic-ui-react";
 import "./App.css";
 
 function App() {
@@ -46,8 +46,150 @@ function App() {
           </Menu.Item>
         </Menu>
       </header>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* Top section */}
+      <div style={{ border: "solid black", padding: "2vw" }}>
+        <Grid stackable columns={1}>
+          <Grid.Column>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </Grid.Column>
+        </Grid>
+      </div>
+      {/* Grid section */}
+      <div>
+        <Grid
+          stackable
+          columns={4}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "1.5vw",
+            paddingTop: "0"
+          }}
+        >
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                border:'solid black'
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/image.png"
+              size="small"
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            />
+          </Grid.Column>
+        </Grid>
       </div>
     </div>
   );
