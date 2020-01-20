afc965190af950e97ee76853b953f6db4fd8fb51
Daniel <danielceballos94@gmail.com>
2020-01-20 10:09:37 -0500

    Centered placeholder React logo

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <Menu stackable>
           <Menu.Item style={{ flexGrow: "1" }}>
             <img
@@ -46,19 +46,8 @@ function App() {
           </Menu.Item>
         </Menu>
       </header>
-      <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </div>
     </div>
   );
