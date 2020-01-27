83306dabef5e3b55b73b173c849c0054f476b2c0
Daniel Ceballos <38518156+shibeknight@users.noreply.github.com>
2020-01-27 13:06:39 -0500

    Feature/10 react app component refactor (#11)
    * Added eslint and peer dependencies with yarn
    
    * Remove eslint with yarn, fixing dependency issues
    
    * Added eslint dependencies, fixed eslintrc
    
    * Fix formatting and code style using eslint
    
    * Create empty view components
    
    * Create Header class component
    
    * Add header menu code
    
    * Add code from App.js
    
    * Add missing imports
    
    * Add Header component and removed unused import
    
    * Removed home code, added route switch
    
    * Remove unused import and add proper ones for routing and views

---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Menu stackable>
+        <Menu.Item style={{ flexGrow: '1' }}>
+          <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
+        </Menu.Item>
+        <Menu.Item name="home" link as={Link} to="/">
+          Home
+        </Menu.Item>
+        <Menu.Item name="profile" link as={Link} to="/profile">
+          Profile
+        </Menu.Item>
+        <Menu.Item name="sign-in" link as={Link} to="/sign-in">
+          Sign in
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Views/Home.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+import Header from '../Components/Header';
+import logo from './logo.svg';
+import './App.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+        {/* Top section */}
+        <div style={{ border: 'solid black', padding: '2vw' }}>
+          <Grid stackable columns={1}>
+            <Grid.Column>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+            </Grid.Column>
+          </Grid>
+        </div>
+        {/* Grid section */}
+        <div>
+          <Grid
+            stackable
+            columns={4}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '1.5vw',
+              paddingTop: '0',
+            }}
+          >
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  border: 'solid black',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+            <Grid.Column
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                size="small"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              />
+            </Grid.Column>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
