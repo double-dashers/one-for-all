560908d3b0b11d4ce4903499588037e9f309d54c
Daniel <danielceballos94@gmail.com>
2020-02-06 14:31:06 -0500

    Add mockup columns

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Container } from 'semantic-ui-react';
+import { Grid, Container, Segment, Image } from 'semantic-ui-react';
 
 class Profile extends Component {
   constructor(props) {
@@ -12,7 +12,16 @@ class Profile extends Component {
     return (
       <Container>
         <Grid stackable columns={2}>
-          Something here
+          <Grid.Column mobile={16} tablet={6} computer={6}>
+            <Segment>
+              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={10} computer={10}>
+            <Segment>
+              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+            </Segment>
+          </Grid.Column>
         </Grid>
       </Container>
     );
