4f1199f3c404d5e3f6341a0848f248f2438cb49f
Daniel <danielceballos94@gmail.com>
2020-02-07 10:37:48 -0500

    Add tab titles

--- src/Components/TabMenu.js
+++ src/Components/TabMenu.js
@@ -3,15 +3,15 @@ import { Tab } from 'semantic-ui-react';
 
 const panes = [
   {
-    menuItem: 'Tab 1',
+    menuItem: 'Activity',
     render: () => <Tab.Pane attached="top">Tab 1 Content</Tab.Pane>,
   },
   {
-    menuItem: 'Tab 2',
+    menuItem: 'Timeline',
     render: () => <Tab.Pane attached="top">Tab 2 Content</Tab.Pane>,
   },
   {
-    menuItem: 'Tab 3',
+    menuItem: 'Settings',
     render: () => <Tab.Pane attached="top">Tab 3 Content</Tab.Pane>,
   },
 ];
