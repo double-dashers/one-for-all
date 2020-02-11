4b4386f659b7fdc87e4d8d52e0317cf86e1e60fa
Daniel <danielceballos94@gmail.com>
2020-02-11 10:55:14 -0500

    Start list card

---
+++ src/Components/CurrentlyCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+const CurrentlyCard = () => {
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>My current list</Card.Header>
+        
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default CurrentlyCard;
