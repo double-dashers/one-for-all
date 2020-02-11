bae4991b9a32342b635308f9bdc040805aee4b92
Daniel <danielceballos94@gmail.com>
2020-02-11 10:56:39 -0500

    Add list with checkbox

--- src/Components/CurrentlyCard.js
+++ src/Components/CurrentlyCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, List, Checkbox } from 'semantic-ui-react';
 
 const CurrentlyCard = () => {
   return (
     <Card>
       <Card.Content>
         <Card.Header>My current list</Card.Header>
-        
+        <List>
+          <List.Item>
+            <Checkbox label="Game #1" />
+          </List.Item>
+        </List>
       </Card.Content>
     </Card>
   );
