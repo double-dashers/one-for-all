155a38e47fdc00cee8a346a9198e52554b9a31da
Daniel <danielceballos94@gmail.com>
2020-02-11 13:16:46 -0500

    Add feed to tabmenu in profile with mock data

--- src/Components/ActivityFeed.js
+++ src/Components/ActivityFeed.js
@@ -34,6 +34,30 @@ const events = [
       'https://react.semantic-ui.com/mages/wireframe/image-text.png',
     ],
   },
+  {
+    date: '1 Hour Ago',
+    image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+    meta: '4 Likes',
+    summary: 'Elliot Fu added you as a friend',
+  },
+  {
+    date: '1 Hour Ago',
+    image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+    meta: '4 Likes',
+    summary: 'Elliot Fu added you as a friend',
+  },
+  {
+    date: '1 Hour Ago',
+    image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+    meta: '4 Likes',
+    summary: 'Elliot Fu added you as a friend',
+  },
+  {
+    date: '1 Hour Ago',
+    image: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+    meta: '4 Likes',
+    summary: 'Elliot Fu added you as a friend',
+  },
 ];
 const ActivityFeed = () => {
   return <Feed events={events} />;
