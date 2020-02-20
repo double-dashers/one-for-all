927bc799151773f142cd86a6d911ab7472260234
Luca <lucahbo@hotmail.com>
2020-02-20 02:26:22 -0500

    Created UserProfileCard

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Grid, Container, Segment, Image } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
 import TabMenu from '../Components/TabMenu';
 import CurrentlyCard from '../Components/CurrentlyCard';
+import UserProfileCard from '../Components/UserProfileCard';
+
 
 class Profile extends Component {
   constructor(props) {
@@ -17,9 +19,7 @@ class Profile extends Component {
           <Grid.Column mobile={16} tablet={6} computer={6}>
             <Grid.Row>
               <Grid.Column mobile={16} tablet={16} computer={16}>
-                <Segment>
-                  <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-                </Segment>
+                <UserProfileCard />
               </Grid.Column>
               <Grid.Column mobile={16} tablet={16} computer={16} style={{ marginTop: '2vw' }}>
                 <CurrentlyCard />
