5dec03002b5d8e1cd1c47b742cdc41f950fc9aa7
Luca <lucahbo@hotmail.com>
2020-02-20 21:27:27 -0500

    Image justified left

--- src/Components/UserProfileCard.js
+++ src/Components/UserProfileCard.js
@@ -3,32 +3,29 @@ import { Button, Card, Icon, Image } from 'semantic-ui-react';
 
 const UserProfileCard = () => {
   return (
-    <Card.Group centered itemsPerRow={2}>
-      <Card>
-        <Image
-          size="small"
-          floated="left"
-          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-          wrapped
-          ui={false}
-        />
-        <Card.Content>
-          <Card.Header>Arturo</Card.Header>
-          <Card.Meta>
-            <span className="position">CEO</span>
-          </Card.Meta>
-          <Card.Description>Arturo is a musician living in CDMX.</Card.Description>
+    <Card centered>
+     
+      <Card.Content>
+      <Image
+        size="small"
+        floated="left"
+        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+      />
+        <Card.Header>Arturo</Card.Header>
+        <Card.Meta>
+          <span className="position">CEO</span>
+        </Card.Meta>
+        <Card.Description>Arturo is a musician living in CDMX.</Card.Description>
 
-          <Button centered color="blue" type="submit">
-            Message
-          </Button>
-        </Card.Content>
-        <Card.Content extra>
-          <Icon name="user" />
-          22 Friends
-        </Card.Content>
-      </Card>
-    </Card.Group>
+        <Button centered color="blue" type="submit">
+          Message
+        </Button>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="user" />
+        22 Friends
+      </Card.Content>
+    </Card>
   );
 };
 
