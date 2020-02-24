8155e6ca64ccc1618e72a5538a0f47c77b0283f0
Daniel <danielceballos94@gmail.com>
2020-02-24 10:22:40 -0500

    Delete home menu item

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,6 @@ class Header extends Component {
         <Menu.Item style={{ flexGrow: '1' }}>
           <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
         </Menu.Item>
-        <Menu.Item name="home">Home</Menu.Item>
         <Menu.Item name="profile">Profile</Menu.Item>
         <Menu.Item name="sign-in">Sign in</Menu.Item>
       </Menu>
