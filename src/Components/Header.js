0df53ccc557edc3bcfec7a2a45a304a45136c3b4
Daniel <danielceballos94@gmail.com>
2020-02-24 10:21:49 -0500

    Remove links, keep navigation buttons on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,15 +15,9 @@ class Header extends Component {
         <Menu.Item style={{ flexGrow: '1' }}>
           <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
         </Menu.Item>
-        <Menu.Item name="home" link as={Link} to="/">
-          Home
-        </Menu.Item>
-        <Menu.Item name="profile" link as={Link} to="/profile">
-          Profile
-        </Menu.Item>
-        <Menu.Item name="sign-in" link as={Link} to="/login">
-          Sign in
-        </Menu.Item>
+        <Menu.Item name="home">Home</Menu.Item>
+        <Menu.Item name="profile">Profile</Menu.Item>
+        <Menu.Item name="sign-in">Sign in</Menu.Item>
       </Menu>
     );
   }
