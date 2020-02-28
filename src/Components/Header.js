2edb803ea7413b3d81b1db2529e0aa31aa8f7986
Daniel Ceballos <38518156+shibeknight@users.noreply.github.com>
2020-02-28 13:13:23 -0500

    Feature/34 delete unused views (#35)
    * Remove home route and make profile default
    
    * Remove links, keep navigation buttons on header
    
    * Remove link import
    
    * Delte home view
    
    * Delete home menu item

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 
 class Header extends Component {
   constructor(props) {
@@ -15,15 +14,8 @@ class Header extends Component {
         <Menu.Item style={{ flexGrow: '1' }}>
           <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
         </Menu.Item>
-        <Menu.Item name="home" link as={Link} to="/">
-          Home
-        </Menu.Item>
-        <Menu.Item name="profile" link as={Link} to="/profile">
-          Profile
-        </Menu.Item>
-        <Menu.Item name="sign-in" link as={Link} to="/login">
-          Sign in
-        </Menu.Item>
+        <Menu.Item name="profile">Profile</Menu.Item>
+        <Menu.Item name="sign-in">Sign in</Menu.Item>
       </Menu>
     );
   }
