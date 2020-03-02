9439a97b2b3abf626d720119c528a7d31ec1e1fb
Daniel <danielceballos94@gmail.com>
2020-03-02 11:08:06 -0500

    Rework to functional component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Menu stackable>
-        <Menu.Item style={{ flexGrow: '1' }}>
-          <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
-        </Menu.Item>
-        <Menu.Item name="profile">Profile</Menu.Item>
-        <Menu.Item name="sign-in">Sign in</Menu.Item>
-      </Menu>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <Menu stackable>
+      <Menu.Item style={{ flexGrow: '1' }}>
+        <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
+      </Menu.Item>
+      <Menu.Item name="profile">Profile</Menu.Item>
+      <Menu.Item name="sign-in">Sign in</Menu.Item>
+    </Menu>
+  );
+};
 
 export default Header;
