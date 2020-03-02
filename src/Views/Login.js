9b4f77c7fa73dd5a8eca35674dfae9c8c2c2bc2f
Daniel <danielceballos94@gmail.com>
2020-03-02 10:42:19 -0500

    Rework to functional component

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,54 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Button, Message, Image, Form, Segment, Header } from 'semantic-ui-react';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
+const Login = () => {
+  return (
+    <div>
+      <Grid
+        textAlign="center"
+        style={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: '450px' }}>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://react.semantic-ui.com/logo.png" /> Log-in to your account
+          </Header>
+          <Form size="large">
+            <Segment>
+              <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              />
 
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Grid
-          textAlign="center"
-          style={{
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: '450px' }}>
-            <Header as="h2" color="teal" textAlign="center">
-              <Image src="https://react.semantic-ui.com/logo.png" /> Log-in to your account
-            </Header>
-            <Form size="large">
-              <Segment>
-                <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Password"
-                  type="password"
-                />
-
-                <Button color="teal" fluid size="large">
-                  Login
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              New to us? <a href="noref">Sign Up</a>
-            </Message>
-          </Grid.Column>
-        </Grid>
-      </div>
-    );
-  }
-}
+              <Button color="teal" fluid size="large">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <a href="noref">Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+};
 
 export default Login;
