e40cfbbef81835d59c983baa0ccb45e07a76f350
Daniel <danielceballos94@gmail.com>
2020-03-02 11:35:57 -0500

    Rework style with jss

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Grid, Container, Segment, Image } from 'semantic-ui-react';
+import { createUseStyles } from 'react-jss';
 import TabMenu from '../Components/TabMenu';
 import CurrentlyCard from '../Components/CurrentlyCard';
 
+const useStyles = createUseStyles({
+  cardCol: {
+    marginTop: '2vw',
+  },
+});
+
 const Profile = () => {
+  const classes = useStyles();
+
   return (
     <Container fluid>
       <Grid stackable columns={2}>
@@ -14,7 +23,7 @@ const Profile = () => {
                 <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
               </Segment>
             </Grid.Column>
-            <Grid.Column mobile={16} tablet={16} computer={16} style={{ marginTop: '2vw' }}>
+            <Grid.Column mobile={16} tablet={16} computer={16} className={classes.cardCol}>
               <CurrentlyCard />
             </Grid.Column>
           </Grid.Row>
