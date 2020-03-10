3165c256ec2caa9935646baa182bed7c0fc892a7
Daniel Ceballos <38518156+shibeknight@users.noreply.github.com>
2020-03-10 09:19:14 -0400

    Feature/37 styles rework (#39)
    * Add react-jss
    * Fix styling
    * Rework styles using JSS

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { createUseStyles } from 'react-jss';
 import Header from './Header';
+
+const useStyles = createUseStyles({
+  main: {
+    padding: '2vw',
+  },
+});
 
 const Layout = props => {
   const { children } = props;
+  const classes = useStyles();
   return (
     <div>
       <Header />
       <div>
-        <main style={{ padding: '2vw' }}>{children}</main>
+        <main className={classes.main}>{children}</main>
       </div>
     </div>
   );
