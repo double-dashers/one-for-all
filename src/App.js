e6500a64be1eb632058d6e08050d1223268ad334
Daniel <danielceballos94@gmail.com>
2020-03-12 11:07:46 -0400

    Comment out profile view import for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Profile from './Views/Profile';
+// import Profile from './Views/Profile';
 import Login from './Views/Login';
 import './App.css';
 import Layout from './Components/Layout';
@@ -13,8 +13,8 @@ function App() {
         <div>
           <Layout>
             <Switch>
-              <Route exact path="/profile" component={Profile} />
               <Route exact path="/" component={Main} />
+              {/* <Route exact path="/profile" component={Profile} /> */}
               <Route exact path="/login" component={Login} />
             </Switch>
           </Layout>
