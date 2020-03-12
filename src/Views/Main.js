7e44302e537e21dc8f5ea78ffddc295d96d7272f
Daniel <danielceballos94@gmail.com>
2020-03-12 11:16:14 -0400

    Add cards with set height and title

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { Grid, Container } from 'semantic-ui-react';
+import { Grid, Container, Card } from 'semantic-ui-react';
 import { createUseStyles } from 'react-jss';
 
+const useStyles = createUseStyles({
+  statusCard: {
+    height: '650px',
+  },
+});
+
 const Main = () => {
+  const classes = useStyles();
   return (
     <Container fluid>
       <Grid stackable columns={4}>
-        <Grid.Column mobile={16} tablet={4} computer={4}></Grid.Column>
-        <Grid.Column mobile={16} tablet={4} computer={4}></Grid.Column>
-        <Grid.Column mobile={16} tablet={4} computer={4}></Grid.Column>
-        <Grid.Column mobile={16} tablet={4} computer={4}></Grid.Column>
+        <Grid.Column mobile={16} tablet={4} computer={4}>
+          <Card raised fluid className={classes.statusCard}>
+            <Card.Content>
+              <Card.Header>Backlog</Card.Header>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={4} computer={4}>
+          <Card raised fluid className={classes.statusCard}>
+            <Card.Content>
+              <Card.Header>Currently</Card.Header>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={4} computer={4}>
+          <Card raised fluid className={classes.statusCard}>
+            <Card.Content>
+              <Card.Header>Dropped</Card.Header>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={4} computer={4}>
+          <Card raised fluid className={classes.statusCard}>
+            <Card.Content>
+              <Card.Header>Completed</Card.Header>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
       </Grid>
     </Container>
   );
