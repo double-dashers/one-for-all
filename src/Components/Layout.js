cfd8075e2678ed16889266fa38e38aa71ca2168c
Daniel Ceballos <38518156+shibeknight@users.noreply.github.com>
2020-03-12 14:11:23 -0400

    Feature/40 mobile nav (#41)
    * Create windowdimensionsprovider
    
    * Wrap app.js in windowdimprovider
    
    * Implement windowDimensions in layout
    
    * Create mobile header with sidebar
    
    * Add children as props
    
    * Implemented mobile menu
    
    * Change menu items and remove unnecessary styles

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss';
+import { useWindowDimensions } from '../utils/WindowDimensionsProvider';
 import Header from './Header';
+import MobileHeader from './MobileHeader';
 
 const useStyles = createUseStyles({
   main: {
@@ -10,20 +12,35 @@ const useStyles = createUseStyles({
 });
 
 const Layout = props => {
-  const { children } = props;
+  const { width } = useWindowDimensions();
+
+  const { children, breakpoint } = props;
   const classes = useStyles();
-  return (
-    <div>
-      <Header />
-      <div>
-        <main className={classes.main}>{children}</main>
-      </div>
-    </div>
-  );
+  let section;
+  if (width > breakpoint) {
+    section = (
+      <>
+        <Header />
+        <div>
+          <main className={classes.main}>{children}</main>
+        </div>
+      </>
+    );
+  } else {
+    section = (
+      <MobileHeader>
+        <div>
+          <main className={classes.main}>{children}</main>
+        </div>
+      </MobileHeader>
+    );
+  }
+  return <div>{section}</div>;
 };
 
 Layout.propTypes = {
   children: PropTypes.element.isRequired,
+  breakpoint: PropTypes.number.isRequired,
 };
 
 export default Layout;
