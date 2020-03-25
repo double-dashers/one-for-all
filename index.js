3ca43049e8f07deef2580b1437912bcce3c51c01
Daniel <danielceballos94@gmail.com>
2020-03-25 13:05:49 -0400

    Add proxy and path to react build

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
